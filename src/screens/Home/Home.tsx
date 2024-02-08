913b10fc769f9cf54844c679e315855cf1d42926
Alexandre Bulcao <ambulcao@gmail.com>
2024-02-08 14:46:01 +0000

    Readme e logo do projeto
    Readme e logo do projeto

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -2,10 +2,12 @@ import React from "react"
 import { Text, View } from 'react-native'
 import { StatusBar } from 'expo-status-bar'
 import HomeStyles from '../Home/HomeStyle'
+import { Menu } from "../../components/Menu/Menu"
 
 export default function Home() {
   return(
     <View style={HomeStyles.container}>
+      <Menu/>
       <Text>Home works!!</Text>
       <StatusBar style="auto" />
     </View>
